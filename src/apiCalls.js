b6bd11bc6e0a889e493b737330b353ffa6324950
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-26 10:05:49 -0600

    add post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,13 @@
 export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+  return fetch('http://localhost:3001/api/v1/urls').then((response) =>
+    response.json()
+  )
+}
+
+export const postUrl = (newUrl) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newUrl)
+  })
 }
