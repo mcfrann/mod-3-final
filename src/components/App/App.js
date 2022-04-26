b8bf4b600d14cd0c09a7111091d33120a4484b89
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-26 10:06:07 -0600

    add newUrl method and componentDidUpdate

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { Component } from 'react'
+import './App.css'
+import { getUrls, postUrl } from '../../apiCalls'
+import UrlContainer from '../UrlContainer/UrlContainer'
+import UrlForm from '../UrlForm/UrlForm'
 
 export class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       urls: []
     }
   }
 
   componentDidMount() {
+    this.fetchUrls()
+  }
+
+  componentDidUpdate() {
+    this.fetchUrls()
+  }
+
+  fetchUrls() {
+    getUrls().then((newUrls) => this.setState(newUrls))
+  }
+
+  addUrl(newUrl) {
+    postUrl(newUrl)
   }
 
   render() {
     return (
-      <main className="App">
+      <main className='App'>
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addUrl={this.addUrl} />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={this.state.urls} />
       </main>
-    );
+    )
   }
 }
 
-export default App;
+export default App
